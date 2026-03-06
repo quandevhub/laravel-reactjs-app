d2bdaaeb58a9c6c9e744870f43541fe141c2cef7
Quan Dev <quandevhub@gmail.com>
2026-03-06 11:01:27 +0700

    edit route

--- laravel-backend/routes/api.php
+++ laravel-backend/routes/api.php
@@ -7,11 +7,11 @@ use App\Http\Controllers\Api\TasksController;
 Route::post('user/register', [AuthController::class, 'register']);
 Route::post('user/login', [AuthController::class, 'login']);
 
-// Route::middleware('auth:sanctum')->group(function () {
+Route::middleware('auth:sanctum')->group(function () {
     Route::post('user/logout', [AuthController::class, 'logout']);
     Route::post('forgot-password', [AuthController::class, 'forgotPassword']);
     Route::post('reset-password', [AuthController::class, 'resetPassword']);
     Route::apiResource('users', UsersController::class);
     Route::apiResource('tasks', TasksController::class);
     Route::get('list-user', [UsersController::class, 'listUser']);
-// });
+});
